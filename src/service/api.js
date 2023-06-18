fcd69d9848516e5bbcc62b44db0848e6b3d63f7e
unknown <juratbekxudayberganov9@gmail.com>
2023-06-18 15:01:58 +0500

    Changes

--- src/service/api.js
+++ src/service/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://62.217.176.186:8080/api";
+const api = axios.create({ baseURL: "http://62.217.176.186:8080/api" });
 
-export default axios;
+export default api;
