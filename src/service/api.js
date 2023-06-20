548ee2da1c73f4eef3cbeb40e9cace94ca0d8e38
unknown <juratbekxudayberganov9@gmail.com>
2023-06-20 10:10:18 +0500

    smallas

--- src/service/api.js
+++ src/service/api.js
@@ -2,6 +2,10 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "https://api.urganchttb.uz/api",
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Content-Type": "application/json",
+  },
 });
 
 export default api;
