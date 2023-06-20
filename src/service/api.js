57d0400482f2c3d8d8faf84251bca561596e875f
unknown <juratbekxudayberganov9@gmail.com>
2023-06-20 10:13:35 +0500

    sasd

--- src/service/api.js
+++ src/service/api.js
@@ -3,7 +3,6 @@ import axios from "axios";
 const api = axios.create({
   baseURL: "https://api.urganchttb.uz/api",
   headers: {
-    "Access-Control-Allow-Origin": "*",
     "Content-Type": "application/json",
   },
 });
