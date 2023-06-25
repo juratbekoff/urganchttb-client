84fddbbd33da1163f73c408a26af0064883743c3
9-zona 7-komp <juratbekxudayberganov9@gmail.com>
2023-06-25 12:15:40 +0500

    change3s

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,8 +5,8 @@ import "./css/header.css";
 const Header = () => {
   return (
     <div className="mt-1">
-      <div className="flex flex-col absolute gap-4 pl-8 left-[10%] top-[40%] max-md:top-[22%] w-[35%] max-md:w-[100%]">
-        <h1 className="font-bold text-white text-3xl max-md:inline-block max-md:text-[14px] max-md:leading-4 max-md:font-medium">
+      <div className="flex flex-col absolute gap-4 pl-8 left-[10%] top-[40%] max-md:top-[19.5%] w-[35%] max-md:w-[80%] max-md:left-0">
+        <h1 className="font-bold text-white text-3xl max-md:text-[20px] max-md:leading-6 max-md:font-medium max-md:text-center max-md:pl-9">
           Urganch Tuman Tibbiyot Birlashmasi!
         </h1>
         <p className="break-normal text-[17px] header_title text-white font-semibold">
