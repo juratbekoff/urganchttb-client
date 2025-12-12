eadf0f50ee1508dfe2efc1e625a2e51c471b79d6
Juratbek <104249426+juratbekoff@users.noreply.github.com>
2025-12-12 11:55:56 +0500

    Update API base URL to use environment variable

--- src/service/api.js
+++ src/service/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://api.urganch-ttb.uz/api",
+  baseURL: import.meta.env.VITE_API_URL!,
   headers: {
     "Access-Control-Allow-Origin": "https://api.urganch-ttb.uz/",
     "Content-Type": "application/json",
